de758a14f56521bffa6ddf087e297248228d9c95
Alberto Zirondelli <ziro.alberto@gmail.com>
2017-08-26 18:10:38 +0200

    Update Consul keys with snapshot data

--- plugin/backup/BackupPlugin.py
+++ plugin/backup/BackupPlugin.py
@@ -1,10 +1,11 @@
 #! /bin/usr/python
 
-import os
+import json
+from datetime import datetime
 
 import consul
+
 from .SanoidBackupPlugin import SanoidBackupPlugin
-from .MockBackupPlugin import MockBackupPlugin, MockKeyValueStore
 
 """
 Generic backup plugin
@@ -15,29 +16,53 @@ Generic backup plugin
 
 
 class BackupPlugin(object):
-    def __init__(self, kv, snapshot_driver):
-        self.kv = kv
+    def __init__(self, cons, snapshot_driver):
+        self.kv = cons.kv
         self.driver = snapshot_driver
+        self.node = cons.agent.self()["Member"]
+
+    def load_config(self):
+        return {
+            "dataset": ["zpool-docker/myapp"]
+        }
 
     def take_snapshot(self):
         self.driver.take_snapshot()
-        self.kv.put('snapshot/myapp', self.get_snapshot())
+
+        dataset = self.load_config()["dataset"][0]
+
+        index, value = self.kv.get('snapshots')
+
+        # TODO: remove all the snapshots removed by this node
+        all_snapshots = value["Value"]
+        if not all_snapshots:
+            # stringa vuota
+            all_snapshots = json.load(all_snapshots)
+        else:
+            all_snapshots = []
+
+        new_snapshot = {
+            "dataset": dataset,
+            "date": datetime.now().timestamp(),
+            "nodo": self.node
+        }
+
+        all_snapshots.insert(0, new_snapshot)
+
+        self.kv.put('snapshots', json.dumps(all_snapshots))
 
     def apply_backup_policy(self, policy):
         self.driver.apply_backup_policy(policy)
 
-    def get_snapshot(self):
+    def get_snapshots(self):
         return self.driver.get_snapshots()
 
     @staticmethod
     def factory():
         driver = SanoidBackupPlugin("/opt/sanoid/sanoid")
-        kv = consul.Consul().kv
+        cons = consul.Consul()
 
-        # driver = MockBackupPlugin()
-        # kv = MockKeyValueStore()
-
-        return BackupPlugin(kv, driver)
+        return BackupPlugin(cons, driver)
 
 
 if __name__ == "__main__":
