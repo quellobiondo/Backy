6a64e9c87e5972a5ba9add475b1f77fd023b6c19
Alberto Zirondelli <ziro.alberto@gmail.com>
2017-08-28 19:44:27 +0200

    BugFixes passaggio parametri Backup

--- configure.py
+++ configure.py
@@ -64,7 +64,7 @@ def take_snapshot(args):
     if args.verbose:
         logger.setLevel(logging.DEBUG)
     logger.debug("Take backup")
-    BackupPlugin.factory().take_snapshot(args.name, args.dataset)
+    BackupPlugin.factory().take_snapshot(args.dataset, args.name)
 
 def check_backups_args(string):
     return string
